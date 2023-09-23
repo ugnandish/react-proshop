91155e98289faf061540f226ac5534f59d5c5171
nandish.ug <nandish.ug@gmail.com>
2023-09-23 18:32:01 +0100

    build changes

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,6 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-});
-
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -36,6 +32,20 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve(); //set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    //any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    ); 
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
